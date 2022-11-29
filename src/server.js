fd7ae76338b3f86bcf263c29715ec03920e98946
CGJI58 <leemsk22@naver.com>
2022-11-29 16:45:54 +0900

    ~#1.4

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import http from "http";
 const PORT = 4000;
 
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 app.set("view engine", "pug");
 app.set("views", `${__dirname}/views`);
@@ -12,9 +14,14 @@ app.use(express.static(`${__dirname}/public`));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  socket.on("message", (message, isBinary) => {
+    message = isBinary ? message : message.toString();
+    console.log("New message from the browser:", message);
+  });
+  socket.send("Hello~~");
+});
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-server.listen(PORT, handleListen);
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
